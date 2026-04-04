fe15d4d057a9910882e801a7532587cbecc751a2
Florian <dev@fwcr.de>
2026-04-04 23:18:11 +0200

    refactoring WIP

--- apps/web/src/app/(legal)/privacy/content.en.tsx
+++ apps/web/src/app/(legal)/privacy/content.en.tsx
@@ -573,7 +573,7 @@ function ServiceTable({ rows }: { rows: ServiceRow[] }) {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.service}>
+            <TableRow key={`${row.service}-${row.country}`}>
               <TableCell>{row.service}</TableCell>
               <TableCell>{row.purpose}</TableCell>
               <TableCell>{row.dataSent}</TableCell>
